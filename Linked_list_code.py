f2476bed3a4960f0965dd4c7f00de5c2d84f241c
Herok4Build <thomash.johnson261@gmail.com>
2022-07-30 17:21:45 -0400

    Updated getting child nodes and searching nodes.

--- Linked_list_code.py
+++ Linked_list_code.py
@@ -13,7 +13,7 @@ class node: # initializing the node class
     def __init__(self,  identifier, parent = None, child = None,):
         self.parent = parent # Set the parent for the node
         self.child = child #set the child for the node
-        self.identifier = identifier # set the identifier for the node
+        self.identifier = identifier.lower() # set the identifier for the node
     
 
     # method to delete the existing node
@@ -25,30 +25,67 @@ class node: # initializing the node class
         self.parent = new_parent
     
     # Continue searching through nodes in the lincked list
-    def continue_search(self, target_identifier,base_node):
-        if base_node.identifier == target_identifier:
-            del base_node
-        else: 
-            base_node.continue_search(target_identifier, base_node.child)
-        
-    def search_nodes(self, target_identifier,root_node):
-        if root_node.identifier == target_identifier:
-            del(root_node)
+    def continue_search(self, target_identifier):
+        #print("The current node identifier: ",self.identifier)
+        #print("The target identifier: ",target_identifier)
+        if self.identifier == target_identifier:
+            print("Found the node.")
+            print(self)
+            return(self)
+        elif(self.get_child() is None):
+            #print("No children detected in sub levels. Returning None.")
+            return("The node with identifier " + target_identifier +" does not exist.")
         else:
-            root_node.continue_search(target_identifier, root_node)
+            child_node = self.get_child()
+            child_node_result = child_node.continue_search(target_identifier)
+            return(child_node_result)
+        
+    def search_nodes(self, target_identifier):
+        target_identifier = target_identifier.lower()
+        #print("The current node identifier: ",self.identifier)
+        #print("The target identifier: ",target_identifier)
+        if self.identifier == target_identifier:
+            return(self)
+        else:
+            if(self.get_child() is None):
+                #print("No children detected top level. Returning response.")
+                return("The node with identifier " + target_identifier +" does not exist.")
+            else:
+                child_node = self.get_child()
+                result_of_search = child_node.continue_search(target_identifier)
+                print("Print result", result_of_search)
+                return(result_of_search)
     
     def create_child(self, child_identifier):
-        self.child = node(identifier = child_identifier, parent = self.identifier)
+        self.child = node(identifier = child_identifier.lower(), parent = self.identifier)
     
     def display_node(self):
-        return("The node is identified as ", self.identifier," with parent ", 
-               self.parent, " and child ", self.child, ".")
+        if(self.child != None):
+            return("The node is identified as ", self.identifier," with parent ", 
+                   self.parent, " and child ", self.child.identifier, ".")
+        else:
+            return("The node is identified as ", self.identifier," with parent ", 
+                   self.parent,".")
+          
+               
+    def get_child(self):
+        return(self.child)
             
 # The start_links() function is used to start running the code.
 def start_links():
     new_node = node(identifier="12145_lone")
     new_node.create_child(child_identifier = "row_243")
+    new_node.child.create_child(child_identifier = "bref_563_c")
+    node_3 = new_node.child.child
+    node_3.create_child(child_identifier = "zoz_54")
+    node_4 = node_3.get_child()
     print(new_node.child.display_node())
+    print(node_3.display_node())
+    print(node_4.parent)
+    print(node_4.display_node())
+    identifier_search_value = "bref_563_c"
+    result_of_search = new_node.search_nodes(identifier_search_value)
+    print("The search for ",identifier_search_value," yielded: ", result_of_search, "with identifier:", result_of_search.identifier,".")
     
     
 start_links()
